da64d5278c23c55722588cd9b28e131e496bf121
Gayatri Srivastava <gayatri.srivastava.cg@gmail.com>
2026-08-19 11:44:19 +0530

    Add files via upload

---
+++ coding.c
@@ -0,0 +1,12 @@
+/* Calculation of simple interest */
+#include <stdio.h>
+int main()
+{
+    int p,n;
+    float r,si;
+    printf("enter value of p,n,r");
+    scanf("%d%d%f",&p,&n,&r);
+    si=p*r*n/100;
+    printf("%f\n",si);
+    return 0;
+}
